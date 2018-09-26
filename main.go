4da93f9155a234da060beba3c6ef446a2a1c5bb1
Chad Barry <zzglitch@hotmail.com>
2018-09-25 18:38:54 -0700

    Get redis keys and average serialized size

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	hostPtr := flag.String("h", "127.0.0.1", "Redis Host")
+	portPtr := flag.Int("p", 6379, "Redis Port")
+	passwordPtr := flag.String("a", "", "Redis password")
+	flag.Parse()
+
+	keyFilter := "*"
+	if len(flag.Args()) > 0 {
+		keyFilter = flag.Args()[0]
+	}
+
+	client := redis.NewClient(
+		&redis.Options{
+			Addr:     *hostPtr + ":" + strconv.Itoa(*portPtr),
+			Password: *passwordPtr,
+			DB:       0,
+		})
+	results, err := client.Keys(keyFilter).Result()
+	if err != nil {
+		fmt.Printf("Error fetching keys: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Number of keys: %v\n", len(results))
+	var totalBytes int
+	regex, _ := regexp.Compile(`serializedlength:(\d*)`)
+	for _, key := range results {
+		val, err := client.DebugObject(key).Result()
+		if err != nil {
+			fmt.Printf("Error getting debug infomration about %s: %v\n", key, err)
+			continue
+		}
+		matches := regex.FindStringSubmatch(val)
+		var bytes int
+		bytes, _ = strconv.Atoi(matches[1])
+		totalBytes += bytes
+	}
+	avgBytes := totalBytes / len(results)
+	fmt.Printf("Average serialized size: %d", avgBytes)
+}
